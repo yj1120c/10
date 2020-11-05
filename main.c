47f253a355a31012087aba579b15d13b8da4972a
yj1120c <70882360+yj1120c@users.noreply.github.com>
2020-11-05 15:51:34 +0900

    02

--- main.c
+++ main.c
@@ -5,12 +5,23 @@
 
 int main(int argc, char *argv[]) {
 	int i = 10;
-	char c = 69;
-	float f = 12.3;
+	char c = 'a';
 	
-	printf("i : %i %p \n", i, &i);
-	printf("c : %c %p \n", c, &c);
-	printf("f : %f %p \n", f, &f);
+	//iptr declaration
+	//pointing to i
+	int *iptr = &i;
+	
+	//cptr declaration
+	//pointing to c
+	char *cptr = &c;
+	
+	//iptr2 declaration
+	//point to what iptr is pointing to
+	int *iptr2 = iptr;
+	
+	printf("i : %p\n%p (size:%i)\n", iptr, &i, sizeof(iptr));
+	printf("c : %p\n%p (size:%i)\n", cptr, &c, sizeof(cptr));
+	printf("iptr2 : %p, %i\n", iptr2, *iptr2);
 	
 	return 0;
 }
